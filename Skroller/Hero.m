7b6e21b171452dcf496f774baa9cd8dccf28b802
ghostFaceKillah <garmulewicz@gmail.com>
2014-10-18 17:05:15 +0200

    Added running girl animation

--- Skroller/Hero.m
+++ Skroller/Hero.m
@@ -28,31 +28,37 @@
 +(Hero *)createHero
 {
     Hero *hero = [[Hero alloc] init];
-    hero.sprite = [SKSpriteNode spriteNodeWithImageNamed:@"girl_one_run_frame1.png"];
+    hero.sprite = [SKSpriteNode spriteNodeWithImageNamed:@"girl0.png"];
 //    hero.sprite = [SKSpriteNode spriteNodeWithImageNamed:@"killer.png"];
   
     // load textures
-    SKTexture *running_1 = [SKTexture textureWithImageNamed:@"girl_one_run_frame1.png"];
-    SKTexture *running_2 = [SKTexture textureWithImageNamed:@"girl_one_run_frame2.png"];
-    SKTexture *running_3 = [SKTexture textureWithImageNamed:@"girl_one_run_frame3.png"];
-    SKTexture *running_4 = [SKTexture textureWithImageNamed:@"girl_one_run_frame4.png"];
+    SKTexture *running_0 = [SKTexture textureWithImageNamed:@"girl0.png"];
+    SKTexture *running_1 = [SKTexture textureWithImageNamed:@"girl1.png"];
+    SKTexture *running_2 = [SKTexture textureWithImageNamed:@"girl2.png"];
+    SKTexture *running_3 = [SKTexture textureWithImageNamed:@"girl3.png"];
+    SKTexture *running_4 = [SKTexture textureWithImageNamed:@"girl4.png"];
+    SKTexture *running_5 = [SKTexture textureWithImageNamed:@"girl5.png"];
+    SKTexture *running_6 = [SKTexture textureWithImageNamed:@"girl6.png"];
+    SKTexture *running_7 = [SKTexture textureWithImageNamed:@"girl7.png"];
+    SKTexture *running_8 = [SKTexture textureWithImageNamed:@"girl8.png"];
+    SKTexture *running_9 = [SKTexture textureWithImageNamed:@"girl9.png"];
     SKTexture *f3 = [SKTexture textureWithImageNamed:@"hero_jump_1.png"];
     SKTexture *f4 = [SKTexture textureWithImageNamed:@"hero_jump_2.png"];
     SKTexture *f5 = [SKTexture textureWithImageNamed:@"hero_jump_3.png"];
 
-//    SKTexture *f1 = [SKTexture textureWithImageNamed:@"killer.png"];
-//    SKTexture *f2 = [SKTexture textureWithImageNamed:@"killer.png"];
-//    SKTexture *f3 = [SKTexture textureWithImageNamed:@"killer.png"];
-//    SKTexture *f4 = [SKTexture textureWithImageNamed:@"killer.png"];
-//    SKTexture *f5 = [SKTexture textureWithImageNamed:@"killer.png"];
-
-    hero.walkTextures = [NSMutableArray arrayWithCapacity:4];
+    hero.walkTextures = [NSMutableArray arrayWithCapacity:10];
     hero.jumpTextures = [NSMutableArray arrayWithCapacity:3];
     
+    [hero.walkTextures addObject:running_0];
     [hero.walkTextures addObject:running_1];
     [hero.walkTextures addObject:running_2];
     [hero.walkTextures addObject:running_3];
     [hero.walkTextures addObject:running_4];
+    [hero.walkTextures addObject:running_5];
+    [hero.walkTextures addObject:running_6];
+    [hero.walkTextures addObject:running_7];
+    [hero.walkTextures addObject:running_8];
+    [hero.walkTextures addObject:running_9];
     [hero.jumpTextures addObject:f3];
     [hero.jumpTextures addObject:f4];
     [hero.jumpTextures addObject:f5];
@@ -61,7 +67,7 @@
     for (SKTexture *t in hero.jumpTextures) {t.filteringMode = SKTextureFilteringNearest;}
     
     // create hero node
-    [Hero animateSprite: hero.sprite : hero.walkTextures : 0.2];
+    [Hero animateSprite: hero.sprite : hero.walkTextures : 0.08 ];
     
     // setup physics
     hero.sprite.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:hero.sprite.size];
