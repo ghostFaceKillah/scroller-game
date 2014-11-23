c880f56f593b185e0aec02c55a5fc70a7c5eed55
MacbookPro <Macbook@MacBook-Johnny.local>
2014-11-23 18:32:54 +0100

    new logo and options menu added

---
+++ Skroller/OptionsScene.h
@@ -0,0 +1,13 @@
+//
+//  optionsScene.h
+//  Skroller
+//
+//  Created by MacbookPro on 21.11.2014.
+//  Copyright (c) 2014 com.mike-dev. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface optionsScene : SKScene
+
+@end
---
+++ Skroller/OptionsScene.m
@@ -0,0 +1,13 @@
+//
+//  optionsScene.m
+//  Skroller
+//
+//  Created by MacbookPro on 21.11.2014.
+//  Copyright (c) 2014 com.mike-dev. All rights reserved.
+//
+
+#import "optionsScene.h"
+
+@implementation optionsScene
+
+@end
